20c2a1cacf53c4c018e63a1d7e480decfda2fe8e
DuryabKhan <khanduryab1@gmail.com>
2023-12-20 02:28:35 +0500

    NavItems done

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,10 +1,35 @@
 "use client";
 
+import { PRODUCT_CATEGORY } from "@/config";
 import React, { useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
   const [activeIndex, setActiveInex] = useState<null | number>(null);
-  return <div className="flex gap-4 h-full"></div>;
+  const isAnyOpen = activeIndex !== null;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORY.map((category, index) => {
+        const handleOpen = () => {
+          if (activeIndex === index) {
+            setActiveInex(null);
+          } else {
+            setActiveInex(index);
+          }
+        };
+        const isOpen = index === activeIndex;
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+            key={category.value}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
---
+++ config/index.tsx
@@ -0,0 +1,47 @@
+
+
+export const PRODUCT_CATEGORY = [
+
+    {
+        label: 'UI Kits',
+        value: 'ui_kits' as const,
+        featured: [
+          {
+            name: 'Editor picks',
+            href: '#',
+            imageSrc: '/nav/ui-kits/mixed.jpg',
+          },
+          {
+            name: 'New Arrivals',
+            href: '#',
+            imageSrc: '/nav/ui-kits/blue.jpg',
+          },
+          {
+            name: 'Bestsellers',
+            href: '#',
+            imageSrc: '/nav/ui-kits/purple.jpg',
+          },
+        ],
+      },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured: [
+            {
+                name:"Icons",
+                href:"#",
+                imageSrc:"/nav/icons/picks.jpg"
+            },
+            {
+                name:"New Arivals",
+                href:"#",
+                imageSrc:"/nav/icons/new.jpg"
+            },
+            {
+                name:"Best Sellers",
+                href:"#",
+                imageSrc:"/nav/icons/bestsellers.jpg"
+            },
+        ]
+    }
+]
